ab8b7098e7c389c1bbb5810597028d2301eba0f2
suqiancheng <1160443855@qq.com>
2018-08-31 17:05:41 +0800

    完善Example03

--- lab-9-chaincode/example_03/example_03.go
+++ lab-9-chaincode/example_03/example_03.go
@@ -36,7 +36,7 @@ type Company struct {
 type Transaction struct {
 	FromType int
 	FormID int
-	ToType string
+	ToType int
 	ToID int
 	Time int64
 	Number int
@@ -70,7 +70,7 @@ func (s *SimpleChainCode) Init(stub *shim.ChaincodeStub,function string, args []
 
 	// 创建一个CenterBank实例，并将其存储到区块链中
 	centerBk := CenterBank{Name:centerBkName,TotalNumber:initalNumber,RestNumber:0,ID:0}
-	err = writeBank(stub,centerBk)
+	err = writeCenterBank(stub,centerBk)
 
 	if err != nil{
 		return nil,errors.New("Failed to store in blockChain"+err.Error())
@@ -91,7 +91,12 @@ func (t *SimpleChainCode) Invoke (stub *shim.ChaincodeStub, function string, arg
 		return t.issueCoin(stub,args)
 	}else if function == "issueCoinToBank"{
 		return t.issueCoinToBank(stub,args)
+	}else if function == "issueCoinToCp"{
+		return t.issueCoinToCp(stub, args)
+	}else if function == "transfer"{
+		return t.transfer(stub, args)
 	}
+	return nil, errors.New("Recieved unknow function invocation")
 
 }
 
@@ -278,6 +283,176 @@ func (t *SimpleChainCode) issueCoinToBank(stub *shim.ChaincodeStub, args []strin
 
 
 /*
+	Bank转账至Company
+	args[0] 商业银行ID
+	args[1] 企业ID
+	args[2] 转账数额
+
+	基本步骤：解析参数、读取数值、转账操作、交易创建、存入数据
+*/
+
+func (t *SimpleChainCode)issueCoinToCp(stub *shim.ChaincodeStub,args []string)([]byte, error){
+	if len(args) != 3{
+		return nil, errors.New("Incorrect Number of arguments.Excepting 3")
+	}
+	var bankID int
+	var cpID int
+	var issuseNum int
+	var company Company
+	var bank Bank
+
+	// 读取传入的参数
+	bankID, err := strconv.Atoi(args[0])
+	if err != nil{
+		return nil, errors.New("Incorrect type of args[0]. Excepting an interger")
+	}
+	cpID, err = strconv.Atoi(args[1])
+	if err != nil{
+		return nil, errors.New("Incorrect type of args[1]. Excepting an interger")
+	}
+	issuseNum, err = strconv.Atoi(args[2])
+	if err != nil{
+		return nil, errors.New("Incorrect type of args[2]. Excepting an interger")
+	}
+
+	// 根据ID，从区块链中读取相应的参数
+	bank,_ , err = getBankByID(stub,bankID)
+	if err != nil {
+		return nil, errors.New("Failed to get bank")
+	}
+
+	company,_ , err = getCompanyById(stub,bankID)
+	if err != nil {
+		return nil, errors.New("Failed to get company")
+	}
+
+	// 进行转账操作
+	if bank.RestNumber < issuseNum{
+		return nil, errors.New("issuseNum is Bigger than RestNumber")
+	}
+
+	bank.RestNumber = bank.RestNumber - issuseNum
+	company.Number = company.Number + issuseNum
+
+	// 将跟新的数据存入到区块链中
+	err = writeBank(stub,bank)
+	if err != nil{
+		return nil ,errors.New("Failed to write bank into blockChain")
+	}
+	err = writeCompany(stub,company)
+	if err != nil{
+		return nil, errors.New("Faile to write company to blockChain" + err.Error())
+		// 对Bank操作进行回滚操作
+		bank, _ ,err = getBankByID(stub,bankID)
+		bank.RestNumber = bank.RestNumber + issuseNum
+		err = writeBank(stub,bank)
+		if err != nil {
+			return nil, errors.New("Failed  to roll back of bank"+ err.Error())
+		}
+	}
+
+	// 若转账成功，则创建交易，记录该笔转账
+	transaction := Transaction{FromType:1,FormID:bankID,ToType:2,ToID:cpID,Time:time.Now().Unix(),Number:issuseNum,ID:transactionNO}
+	err = writeTransaction(stub, transaction)
+	if err != nil{
+		return nil, errors.New("Failed to write transaction into BlockChain")
+	}
+	transactionNO = transactionNO + 1
+
+	tsBytes, err := json.Marshal(transaction)
+	if err != nil{
+		return nil, errors.New("Failed to marshal transaction"+err.Error())
+	}
+	return tsBytes,nil
+}
+
+
+/* transfer 函数，用来在Company之间进行转账
+	request函数
+		- args[0] 转账用户ID
+		- args[1] 被转账用户ID
+		- args[2] 转账余额
+基本思路：
+	读取参数，读取账号、进行转账、存入数据、创建交易、存储交易、返回函数值
+
+*/
+func (t *SimpleChainCode) transfer(stub *shim.ChaincodeStub, args []string)([]byte, error)  {
+	if len(args) != 3 {
+		return nil, errors.New("Incorrect number of argumets. Excepting 3")
+	}
+
+	var cpFrom Company
+	var cpTO Company
+	var cpFromId string
+	var cpFromIdInt int
+	var cpToId string
+	var cpToIdInt int
+	var issueNumber int
+	var tsBytes[] byte
+	var err error
+
+	cpFromId = args[0]
+	cpFromIdInt, err = strconv.Atoi(cpFromId)
+	if err != nil{
+		return nil, errors.New("want integer ~")
+	}
+	cpToId = args[1]
+	cpToIdInt, err = strconv.Atoi(cpToId)
+	if err != nil{
+		return nil, errors.New("want integer ~")
+	}
+	issueNumber,err = strconv.Atoi(args[2])
+	if err != nil {
+		return nil, errors.New("Expecting integer value for asset holding")
+	}
+	cpFrom,_,err = getCompanyById(stub,cpFromIdInt)
+	if err != nil {
+		return nil,errors.New("get errors")
+	}
+	if cpFrom.Number<issueNumber{
+		return nil,errors.New("Not enough money")
+	}
+	cpTO,_,err = getCompanyById(stub,cpToIdInt)
+	if err != nil {
+		return nil,errors.New("get errors")
+	}
+
+	cpFrom.Number = cpFrom.Number - issueNumber
+	cpTO.Number = cpTO.Number + issueNumber
+
+	err = writeCompany(stub,cpFrom)
+	if err != nil {
+		cpFrom.Number = cpFrom.Number - issueNumber
+		cpTO.Number = cpTO.Number + issueNumber
+		return nil, errors.New("write Error" + err.Error())
+	}
+	err = writeCompany(stub,cpTO)
+	if err != nil {
+		cpFrom.Number = cpFrom.Number - issueNumber
+		cpTO.Number = cpTO.Number + issueNumber
+		err = writeCompany(stub,cpFrom)
+		if err !=nil{
+			return nil,errors.New("roll down error")
+		}
+		return nil, errors.New("write Error" + err.Error())
+	}
+	transaction := Transaction{FromType:2,FormID:cpFromIdInt,ToType:2,ToID:cpToIdInt,Time:time.Now().Unix(),Number:issueNumber,ID:transactionNO}
+	err = writeTransaction(stub,transaction)
+	if err != nil {
+		return nil, errors.New("write Error" + err.Error())
+	}
+
+	tsBytes,err = json.Marshal(&transaction)
+	if err != nil {
+		fmt.Println("Error unmarshalling centerBank")
+	}
+
+	transactionNO = transactionNO +1
+	return tsBytes, nil
+
+}
+
+/*
 写一系列的get 和 set 函数， 用于从区块链中存入和读取数据。
 */
 func getCenterBank(stub *shim.ChaincodeStub) (CenterBank, []byte, error){
@@ -294,7 +469,28 @@ func getCenterBank(stub *shim.ChaincodeStub) (CenterBank, []byte, error){
 */
 
 func getBankByID(stub *shim.ChaincodeStub, bankID int)(Bank, []byte, error){
+	var bank Bank
+	bankBytes, err := stub.GetState("bank"+strconv.Itoa(bankID))
+	if err != nil{
+		fmt.Println("Error retrieving cbBytes")	}
+	err = json.Unmarshal(bankBytes,bank)
+	if err != nil{
+		fmt.Printf("Error retrieving cbBytes")
+	}
+	return bank, bankBytes,nil
+}
 
+func getCompanyById(stub *shim.ChaincodeStub,cpId int)(Company, []byte, error){
+	var company Company
+	cpBytes, err := stub.GetState("company"+strconv.Itoa(cpId))
+	if err != nil{
+		fmt.Println("Failed to get company" + err.Error())
+	}
+	err = json.Unmarshal(cpBytes,company)
+	if err != nil{
+		fmt.Println("Error Unmarshal company")
+	}
+	return company, cpBytes, nil
 }
 
 func writeCenterBank(stub *shim.ChaincodeStub,centerBank CenterBank) (error) {
@@ -348,4 +544,8 @@ func writeTransaction(stub *shim.ChaincodeStub, transaction Transaction) (error)
 }
 
 func main(){
+	err := shim.Start(new(SimpleChainCode))
+	 if err != nil{
+	 	fmt.Printf("Error starting Simple chainCode:%s", err)
+	 }
 }
