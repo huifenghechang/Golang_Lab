e18fcea4a9d115b9e31fcd30ad7037644b62a2d6
suqiancheng <1160443855@qq.com>
2018-09-06 15:15:12 +0800

    interface类型断言

---
+++ lab-11-goLang-video/chapter5/interface.md
@@ -0,0 +1,19 @@
+# 类型断言
+
+在Go语言中，对接口进行类型断言，一共有两种方法
+
+
+`
+var k = 8
+
+// 方法1: comma-ok
+if value , ok := k.(Int) ok{
+    fmt.printf("the type of k is Int")
+}
+
+// 方法2: Switch 测试
+switch value = k.(type){
+    case int: //
+    case string: //
+}
+`
--- lab-11-goLang-video/chapter5/main.go
+++ lab-11-goLang-video/chapter5/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"lab-11-goLang-video/chapter5/mock"
+	"lab-11-goLang-video/chapter5/real"
+	"time"
 )
 
 // 定义三个接口
@@ -42,12 +44,36 @@ func session(s RetrieverPoster) string {
 
 func main() {
 	// Retriever 是一个接口
-	var r Retriever
+	var r1 Retriever
+
 	mockRetriever := mock.Retriever{
 		Contents: "this is a fake imooc.com"}
 
-	r = &mockRetriever
+	// 因为接口，本身是一个指针，所给给接口赋值的时候，需要将结构体转换为地址形式
+	r1 = &mockRetriever
+	fmt.Println(r1.Get(url))
 
-	fmt.Println(r.Get(url))
+	var r2 Retriever
 
+	r2 = &real.Retriever{
+		UserAgent: "Mozilla/5.0",
+		TimeOut:   time.Minute,
+	}
+
+	inspect(r2)
+
+}
+
+func inspect(r Retriever) {
+	fmt.Println("Inspecting", r)
+	fmt.Printf("> Type:%T Value:%v \n", r, r)
+	fmt.Printf("> Tpye Switch")
+	switch v := r.(type) {
+	case *mock.Retriever:
+		fmt.Println("Contents:", v.Contents)
+	case *real.Retriever:
+		fmt.Println("Contents", v.UserAgent)
+	}
+
+	fmt.Println()
 }
